93f7aab3fe85f2c66849b0fd900a6f4c35b8cdb1
Moin Ahmed <cmoinahmed@gmail.com>
2025-01-20 17:08:33 +0530

    Game Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'exam2';
+  gameNumbers: number[] = [];
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(number: number) {
+    this.gameNumbers.push(number);
+    if(number % 2 === 0) {
+      this.evenNumbers.push(number);
+    } else {
+      this.oddNumbers.push(number);
+    }
+  }
 }
